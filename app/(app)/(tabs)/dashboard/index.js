a8ee3e181571b73a8bc15c2b50168ad479426a56
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-13 05:04:52 +0600

    update

--- app/(app)/(tabs)/dashboard/index.js
+++ app/(app)/(tabs)/dashboard/index.js
@@ -1,12 +1,24 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useContext } from "react";
+import { Button, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { signOut } from "firebase/auth";
+import { FIREBASE_AUTH } from "../../../../config/firebaseConfig";
+import { AuthContext } from "../../../../contexts/authContext";
 
 const Dashboard = () => {
+  const { setUser } = useContext(AuthContext);
+  const handleSignout = () => {
+    signOut(FIREBASE_AUTH)
+      .then(() => {
+        setUser(null);
+      })
+      .catch((error) => {});
+  };
   return (
     <SafeAreaView>
       <View>
         <Text>This is Dashboard</Text>
+        <Button title="Logout" onPress={handleSignout} />
       </View>
     </SafeAreaView>
   );
