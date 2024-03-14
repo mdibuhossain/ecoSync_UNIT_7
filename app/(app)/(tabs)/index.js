ae908eacfe2a063faee5529382cad39ffe3f4ee6
Md. Ibrahim Hossain <ibuhossain3@gmail.com>
2024-03-15 01:51:21 +0600

    update

--- app/(app)/(tabs)/index.js
+++ app/(app)/(tabs)/index.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { ScrollView, StyleSheet, View, FlatList, Image } from "react-native";
+import {
+  ScrollView,
+  StyleSheet,
+  View,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from "react-native";
 import { TouchableRipple, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import RippleBtn from "../../components/RippleBtn";
@@ -10,38 +17,97 @@ const index = () => {
   const [categories, setCategories] = React.useState([]);
   const [menu, setMenu] = React.useState([]);
   const [filteredMenu, setFilteredMenu] = React.useState([]);
+  const [totalCost, setTotalCost] = React.useState(0);
+  const [estimate, setEstimate] = React.useState({});
 
   React.useEffect(() => {
     const fetchData = async () => {
       setMenu(DataPath);
       setFilteredMenu(DataPath);
-      setCategories([...new Set(DataPath.map((obj) => obj?.category))]);
+      const tmp = [...new Set(DataPath.map((obj) => obj?.category))];
+      setCategories(tmp);
     };
     fetchData();
   }, []);
 
   const handleFilterCategory = (e) => {
-    setFilteredMenu([...menu?.filter((obj) => obj?.category === e)]);
+    const tmp = [...menu?.filter((obj) => obj?.category === e)];
+    setFilteredMenu(tmp);
   };
 
+  const handleCount = (cat) => {
+    setTotalCost(totalCost + cat?.price);
+    let cashMemo = { ...estimate };
+    cashMemo[cat.name] =
+      cashMemo[cat.name] !== undefined ? cashMemo[cat.name] + 1 : 1;
+    setEstimate(cashMemo);
+    console.log(cashMemo);
+  };
   const RenderItem = ({ cat }) => {
-    console.log(cat);
     return (
       <View key={cat.index} style={styles.individualMenuItemContainer}>
-        {/* <TouchableRipple
-          onPress={() => {}}
-          rippleColor="rgba(200, 200, 200, 0.5)"
+        <TouchableOpacity
+          onPress={() => handleCount(cat)}
+          activeOpacity={0.6}
           style={styles.menuItem}
         >
-          <Image
-            resizeMode="contain"
-            source={{
-              uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Wikimedia-logo.png/768px-Wikimedia-logo.png",
+          <View
+            style={{
+              flex: 1,
+              position: "relative",
+              flexDirection: "column",
+              backgroundColor: "white",
             }}
-            style={{ height: "100%", width: "100%" }}
-          />
-        </TouchableRipple> */}
-        <Text>{cat?.name}</Text>
+          >
+            <View
+              style={{
+                flex: 1,
+                width: "100%",
+                height: "100%",
+                textAlign: "center",
+                justifyContent: "center",
+                alignItems: "center",
+                paddingHorizontal: 2,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 16,
+                  textAlign: "center",
+                  fontWeight: "bold",
+                }}
+              >
+                {cat?.name}
+              </Text>
+            </View>
+            <View style={{ flex: 3, marginBottom: 8 }}>
+              <Image
+                resizeMode="contain"
+                source={require("../../../assets/Mixed Chowmein.png")}
+                style={{ height: "100%", width: "100%" }}
+              />
+            </View>
+            <View
+              style={{
+                flex: 1,
+                backgroundColor: "#f1f0ff",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontSize: 16,
+                }}
+              >
+                ৳{cat?.price}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
       </View>
     );
   };
@@ -49,7 +115,7 @@ const index = () => {
   return (
     <SafeAreaView style={{ padding: 10 }}>
       <View style={styles.estimateDisplay}>
-        <Text style={styles.innerEstimateBox}>Calculation</Text>
+        <Text style={styles.innerEstimateBox}>{totalCost}</Text>
       </View>
       {/* Category list */}
       <View style={{ marginVertical: 10 }}>
@@ -67,7 +133,7 @@ const index = () => {
       </View>
 
       <FlatList
-        numColumns={3}
+        numColumns={2}
         data={filteredMenu}
         style={styles.menuContainer}
         renderItem={(cat) => <RenderItem key={cat.index} cat={cat.item} />}
@@ -93,21 +159,20 @@ const styles = StyleSheet.create({
   },
   individualMenuItemContainer: {
     flex: 1,
-    height: 100,
+    height: 150,
+    maxWidth: 100 / 2 + "%",
     backgroundColor: "rgb(231, 231, 231)",
     borderRadius: 5,
     marginHorizontal: 5,
     marginBottom: 10,
     overflow: "hidden",
     borderRadius: 10,
-    elevation: 2,
+    elevation: 3,
   },
   menuItem: {
-    padding: 10,
+    flex: 1,
     width: "100%",
     height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
